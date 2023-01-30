3ccb5d209822188b70faf8576204b13beb3edcfa
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-01-30 22:40:58 +0200

    Link dark theme files in the main dart file and making some style in it

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:marasi_news_app/consts/theme_data.dart';
+import 'package:marasi_news_app/provider/dark_theme_provider.dart';
 import 'package:marasi_news_app/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp( MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeProvider.setDarkTheme =
+      await themeProvider.darkThemePrefs.getTheme();
+  }
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    bool _isDark = true;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: Styles.themeData(_isDark, context),
-        home: HomeScreen());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_){
+          return themeProvider;
+        })
+      ],
+      child: Consumer<DarkThemeProvider>(
+        builder: (context ,themeProvider , child ) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: Styles.themeData(themeProvider.getDarkTheme, context),
+              home: HomeScreen());
+        }
+      ),
+    );
   }
 }
