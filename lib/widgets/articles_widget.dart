8c8f94dc5e81e86cc5c73606afb32d9bf39181c1
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-02-13 20:57:39 +0200

    add text to articles Widget.

--- lib/widgets/articles_widget.dart
+++ lib/widgets/articles_widget.dart
@@ -15,66 +15,67 @@ class ArticleWidget extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Material(
         color: Theme.of(context).cardColor,
-        child: Stack(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: Positioned(
+        child: InkWell(
+          onTap: () {
+          },
+          child: Stack(
+            children: [
+              Positioned(
                 child: Container(
                   height: 60,
                   width: 60,
                   color:Theme.of(context).colorScheme.secondary,
                 ),
               ),
-            ),
-            Positioned(
-              right: 0,
-              bottom: 0,
-              child: Container(
-                height: 60,
-                width: 60,
-                color:Theme.of(context).colorScheme.secondary,
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  color:Theme.of(context).colorScheme.secondary,
+                ),
               ),
-            ),
-            Container(
-              color: Theme.of(context).cardColor,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.all(10),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: FancyShimmerImage(
-                      height: size.height * 0.12,
-                      width: size.width * 0.12,
-                      boxFit: BoxFit.fill,
-                        imageUrl: 'https://images.pexels.com/photos/12071970/pexels-photo-12071970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+              Container(
+                color: Theme.of(context).cardColor,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: FancyShimmerImage(
+                        height: size.height * 0.12,
+                        width: size.width * 0.12,
+                        boxFit: BoxFit.fill,
+                          imageUrl: 'https://images.pexels.com/photos/12071970/pexels-photo-12071970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Title '*10, style: smallTextStyle,),
-                        const VerticalSpacing(5),
-                        Align(alignment: Alignment.topRight,
-                            child: Text("Reading Time",style: smallTextStyle,)),
-                        Row(
-                          children: [IconButton(onPressed: () {},
-                              icon:const Icon(Icons.link_outlined),color: Colors.blue,),
-                            Text('15-2-2023'*2, style: smallTextStyle,)
-                          ],
-                        )
-                      ],
+                    const SizedBox(width: 10,),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Title '*10, textAlign: TextAlign.justify, maxLines: 2 , overflow: TextOverflow.ellipsis, style: smallTextStyle,),
+                          const VerticalSpacing(5),
+                          Align(alignment: Alignment.topRight,
+                              child: Text("Reading Time",style: smallTextStyle,)),
+                          Row(
+                            children: [IconButton(onPressed: () {},
+                                icon:const Icon(Icons.link_outlined),color: Colors.blue,),
+                              Text('15-2-2023'*2,maxLines: 1, style: smallTextStyle,)
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
 
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
