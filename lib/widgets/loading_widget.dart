80285231acd47c63c8dca40e876f2ba14ee8d281
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-02-16 23:50:36 +0200

    shimmer effect for top trending widget .

--- lib/widgets/loading_widget.dart
+++ lib/widgets/loading_widget.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:marasi_news_app/services/utils.dart';
@@ -27,7 +28,24 @@ class _LoadingWidgetState extends State<LoadingWidget> {
   @override
   Widget build(BuildContext context) {
     Size size = Utils(context).getScreenSize;
-    return Expanded(
+    return widget.newsType == NewsType.topTrending
+        ? Swiper(
+      autoplayDelay: 8000,
+      autoplay: true,
+      itemWidth: size.width * 0.9,
+      layout: SwiperLayout.STACK,
+      viewportFraction: 0.9,
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return TopTrendingLoadingWidget(
+            baseShimmerColor: baseShimmerColor,
+            highlightShimmerColor: highlightShimmerColor,
+            size: size,
+            widgetShimmerColor: widgetShimmerColor,
+            borderRadius: borderRadius);
+      },
+    ):
+      Expanded(
       child: ListView.builder(
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
@@ -40,7 +58,81 @@ class _LoadingWidgetState extends State<LoadingWidget> {
 
   }
 }
+class TopTrendingLoadingWidget extends StatelessWidget {
+  const TopTrendingLoadingWidget({
+    Key? key,
+    required this.baseShimmerColor,
+    required this.highlightShimmerColor,
+    required this.size,
+    required this.widgetShimmerColor,
+    required this.borderRadius,
+  }) : super(key: key);
 
+  final Color baseShimmerColor;
+  final Color highlightShimmerColor;
+  final Size size;
+  final Color widgetShimmerColor;
+  final BorderRadius borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        // height: size.height * 0.45,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Shimmer.fromColors(
+          baseColor: baseShimmerColor,
+          highlightColor: highlightShimmerColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  height: size.height * 0.33,
+                  width: double.infinity,
+                  color: widgetShimmerColor,
+                ),
+              ),
+              // Title
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: size.height * 0.06,
+                    decoration: BoxDecoration(
+                      borderRadius: borderRadius,
+                      color: widgetShimmerColor,
+                    ),
+                  )),
+              // Date
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    height: size.height * 0.025,
+                    width: size.width * 0.4,
+                    decoration: BoxDecoration(
+                      borderRadius: borderRadius,
+                      color: widgetShimmerColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 class ArticlesShimmerEffectWidget extends StatelessWidget {
   const ArticlesShimmerEffectWidget({
     super.key,
